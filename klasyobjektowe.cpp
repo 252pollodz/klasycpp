b7ac25478b52bc65902670ddad8dea3bcc8994be
student <student@p.lodz.pl>
2025-06-02 14:01:01 +0200

    klasy4added

--- klasyobjektowe.cpp
+++ klasyobjektowe.cpp
@@ -4,22 +4,28 @@ using namespace std;
 
 int main()
 {
-    //Point p1, p2, p3;
-    Point p1("P1", 0, 90);
-    Point p2("P2", 30, 40);
-    Point p3("P3", 50, 100);
-    p1.show();
-    p2.show();
-    p3.show();
+    Point points[10] = {
+        Point("P1", 0, 0),
+        Point("P2", 10, 20),
+        Point("P3", 30, 40),
+        Point("P4", 50, 60),
+        Point("P8", 70, 80),
+      };
 
-    cout << "Odleglosc pomiedzy punktami p1 i p2 wynosi  " << p1.distance(p2) << endl;
-    const Point& p = p1.distant(p2);
-    cout << "Punkt bardziej oddalonny od poczatku ukladu wspolrzednych to: " << endl;
-    p.show();
-    cout << "Odleglosc pomiedzy punktami p1 i p3 wynosi  " << p1.distance(p3) << endl;
-    const Point& p_second = p1.distant(p3);
-    cout << "Punkt bardziej oddalonny od poczatku ukladu wspolrzednych to: " << endl;
-    p_second.show();
+    cout << "Istnieje " << Point::numberOfPoints() << " punktow."<< endl;
 
+    for (int i = 0; i < 10; i++)
+    {
+        points[i].show();
+    }
+
+
+    {
+        Point p1, p2, p3;
+        cout << endl;
+        cout << "Teraz istnieje " << Point::numberOfPoints() << " punktow." << endl;
+    }
+
+    cout << "A teraz istnieje " << Point::numberOfPoints() << " punktow." << endl;
 
 }

4216dd09735a8ab07e160f42d41dbef3d7565116
student <student@p.lodz.pl>
2025-06-02 13:51:40 +0200

    klasy3added

--- klasyobjektowe.cpp
+++ klasyobjektowe.cpp
@@ -1,29 +1,25 @@
 #include <iostream>
-
-
-#include <iostream>
 #include "Point.h"
 using namespace std;
 
 int main()
 {
     //Point p1, p2, p3;
-    Point p1;
+    Point p1("P1", 0, 90);
     Point p2("P2", 30, 40);
-    Point p3 = Point("P3",5,5);
-    Point p4 = { "P4", 30, 40 };
-    Point p5 {"P5"};
-
-    p1.setName("P1");
-    p1.setXY(10, 20);
-    p2.setXY(30, 40);
-    p5.setXY(70,22);
+    Point p3("P3", 50, 100);
     p1.show();
     p2.show();
     p3.show();
-    p4.show();
-    p5.show();
 
-    p2 = p1;
-    p2.show();
+    cout << "Odleglosc pomiedzy punktami p1 i p2 wynosi  " << p1.distance(p2) << endl;
+    const Point& p = p1.distant(p2);
+    cout << "Punkt bardziej oddalonny od poczatku ukladu wspolrzednych to: " << endl;
+    p.show();
+    cout << "Odleglosc pomiedzy punktami p1 i p3 wynosi  " << p1.distance(p3) << endl;
+    const Point& p_second = p1.distant(p3);
+    cout << "Punkt bardziej oddalonny od poczatku ukladu wspolrzednych to: " << endl;
+    p_second.show();
+
+
 }
